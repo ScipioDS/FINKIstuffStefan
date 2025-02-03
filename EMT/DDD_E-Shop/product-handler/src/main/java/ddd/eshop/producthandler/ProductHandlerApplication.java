c61a035e4dca278a16eaeb2ce6124543f0d7d71c
stojanovs <stefanstojanov6@gmail.com>
2025-02-03 22:13:22 +0100

    Added DDD Project

---
+++ EMT/DDD_E-Shop/product-handler/src/main/java/ddd/eshop/producthandler/ProductHandlerApplication.java
@@ -0,0 +1,13 @@
+package ddd.eshop.producthandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductHandlerApplication.class, args);
+	}
+
+}
